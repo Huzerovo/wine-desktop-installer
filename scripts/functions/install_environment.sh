f0e49a0965434087711e338d9daefa6a0d8f847d
Huzerovo <huzerovo@outlook.com>
2024-10-30 18:07:25 +0800

    fix: 修复安装环境变量错误

--- scripts/functions/install_environment.sh
+++ scripts/functions/install_environment.sh
@@ -3,7 +3,7 @@ install_environment() {
   warn "This action may require sudo privilege"
   if [[ -f "${env_file}" ]]; then
     sudo mkdir -p "/etc/environment.d"
-    sudo cp "${env_file}" "/etc/environment.d/${env_file}"
+    sudo cp "${env_file}" "/etc/environment.d/"
   else
     warn "Can not find environment file, ignored."
   fi
