332c9200f9703c73222fb1692dab88a1e2730742
Huzerovo <huzerovo@outlook.com>
2024-10-30 10:55:13 +0800

    fix: 修复chmod错误

--- scripts/functions/install_start_bin.sh
+++ scripts/functions/install_start_bin.sh
@@ -1,5 +1,5 @@
 install_start_bin() {
-  chmod +x /usr/local/bin/wine-desktop
+  chmod +x wine-desktop
   warn "Installing wine-desktop, it may require sudo privilege"
   sudo cp wine-desktop /usr/local/bin/wine-desktop
 }
