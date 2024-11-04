7fa35cd5db214c83aeb8bf365bf2da72a35f7ba1
Huzerovo <huzerovo@outlook.com>
2024-11-05 00:02:31 +0800

    fix: 修复rc文件安装错误的问题

--- scripts/functions/install_boxrc.sh
+++ scripts/functions/install_boxrc.sh
@@ -1,11 +1,19 @@
 install_boxrc() {
-  if [[ -f "${rc_box64}" ]]; then
-    info "Installing box64 rc file"
-    cp "${rc_box64}" "$HOME/.${rc_box64}"
+  if [[ -n "$version_box64" ]]; then
+    if [[ -f "${rc_box64}" ]]; then
+      info "Installing box64 rc file"
+      cp "${rc_box64}" "$HOME/.box64rc"
+    fi
+  else
+    warn "Ignored box64"
   fi
-  if [[ -f "${rc_box86}" ]]; then
-    info "Installing box86 rc file"
-    cp "${rc_box86}" "$HOME/.${rc_box86}"
+  if [[ -n "$version_box86" ]]; then
+    if [[ -f "${rc_box86}" ]]; then
+      info "Installing box86 rc file"
+      cp "${rc_box86}" "$HOME/.box86rc"
+    fi
+  else
+    warn "Ignoed box86"
   fi
 }
 
